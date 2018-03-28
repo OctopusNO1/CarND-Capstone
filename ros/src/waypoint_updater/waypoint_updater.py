23ac394168c058870ed99abf4d3402c16d371e8e
Ian Burris <ian.burris@gmail.com>
2018-03-27 17:39:52 -0700

    Initial commit of waypoint updater logic

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -3,7 +3,7 @@
 import rospy
 from geometry_msgs.msg import PoseStamped
 from styx_msgs.msg import Lane, Waypoint
-
+from tf.transformations import euler_from_quaternion
 import math
 
 '''
@@ -23,8 +23,8 @@ TODO (for Yousuf and Aaron): Stopline location for each traffic light.
 
 LOOKAHEAD_WPS = 200 # Number of waypoints we will publish. You can change this number
 
-
 class WaypointUpdater(object):
+
     def __init__(self):
         rospy.init_node('waypoint_updater')
 
@@ -33,20 +33,19 @@ class WaypointUpdater(object):
 
         # TODO: Add a subscriber for /traffic_waypoint and /obstacle_waypoint below
 
-
         self.final_waypoints_pub = rospy.Publisher('final_waypoints', Lane, queue_size=1)
 
         # TODO: Add other member variables you need below
+        self.vehicle = None
+        self.waypoints = None
 
-        rospy.spin()
+        self.looper()
 
     def pose_cb(self, msg):
-        # TODO: Implement
-        pass
+        self.vehicle = msg
 
     def waypoints_cb(self, waypoints):
-        # TODO: Implement
-        pass
+        self.waypoints = waypoints.waypoints
 
     def traffic_cb(self, msg):
         # TODO: Callback for /traffic_waypoint message. Implement
@@ -55,6 +54,70 @@ class WaypointUpdater(object):
     def obstacle_cb(self, msg):
         # TODO: Callback for /obstacle_waypoint message. We will implement it later
         pass
+
+    # This is our main loop which is run at a set interval
+    def looper(self):
+        rate = rospy.Rate(10)
+        while not rospy.is_shutdown():
+            if self.vehicle is not None and self.waypoints is not None:
+                # If you turn around around in the simulator all the waypoint can end
+                # up behind the vehicle which will cause nearest_forward_waypoint to
+                # return None
+                start_index = self.nearest_forward_waypoint()
+                if start_index is not None:
+                    end_index = start_index + LOOKAHEAD_WPS
+                    lane_waypoints = []
+                    # For simplicity we can assume we will always drive forward. The
+                    # following code doesn't work if you try to turn around since it
+                    # will find the correct starting waypoint but select the waypoint
+                    # behind you.
+                    if end_index > len(self.waypoints):
+                        # Need to wrap to front of waypoint list
+                        end_index = end_index - len(self.waypoints)
+                        lane_waypoints = self.waypoints[start_index:] + self.waypoints[:end_index]
+                    else:
+                        lane_waypoints = self.waypoints[start_index:end_index]
+                    lane = Lane()
+                    lane.waypoints = lane_waypoints
+                    self.final_waypoints_pub.publish(lane)
+            rate.sleep()
+
+    # Returns the index of the nearest waypoint ahead of the vehicle
+    def nearest_forward_waypoint(self):
+        vehicle_x = self.vehicle.pose.position.x
+        vehicle_y = self.vehicle.pose.position.y
+        vehicle_orientation = self.vehicle.pose.orientation
+        quaternion = (
+            vehicle_orientation.x,
+            vehicle_orientation.y,
+            vehicle_orientation.z,
+            vehicle_orientation.w
+        )
+        vehicle_yaw = euler_from_quaternion(quaternion)[2]
+
+        # Waypoints converted to vehicle coordinates
+        waypoints_transformed = []
+        for waypoint in self.waypoints:
+            waypoint_x = waypoint.pose.pose.position.x
+            waypoint_y = waypoint.pose.pose.position.y
+            x_adj = waypoint_x - vehicle_x
+            y_adj = waypoint_y - vehicle_y
+            x_transformed = x_adj * math.cos(vehicle_yaw) + y_adj * math.sin(vehicle_yaw)
+            y_transformed = -x_adj * math.sin(vehicle_yaw) + y_adj * math.cos(vehicle_yaw)
+            waypoints_transformed.append((x_transformed, y_transformed))
+
+        # Find closest waypoint ahead of vehicle
+        min_distance = None
+        index = None
+        for i, waypoint in enumerate(waypoints_transformed):
+            x, y = waypoint
+            if x > 0:
+                distance = math.sqrt(x * x + y * y)
+                if min_distance is None or distance < min_distance:
+                    min_distance = distance
+                    index = i
+
+        return index
 
     def get_waypoint_velocity(self, waypoint):
         return waypoint.twist.twist.linear.x
