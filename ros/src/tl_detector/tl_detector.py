332050c39d250dfe4b12353db425f63b15026562
student <pereiraclifton@gmail.com>
2018-04-05 19:35:07 -0700

    fixed indents from IDE

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -133,31 +133,26 @@ class TLDetector(object):
 
         """
         closest_light = None
-		light_wp_idx = None
-				
+	light_wp_idx = None
         # List of positions that correspond to the line to stop in front of for a given intersection
         stop_line_positions = self.config['stop_line_positions']
         if(self.pose):
-            
-			car_wp_idx = self.get_closest_waypoint(self.pose.pose.position.x, self.pose.pose.position.y)
-
-			#TODO find the closest visible traffic light (if one exists)
-			diff = len(self.waypoints.waypoints)
-			for i, light in enumerate(self.ligths):
-				#Get stop line waypoint index
-				line = stop_line_positions[i]
-				temp_wp_idx = self.get_closest_waypoint(line[0], line[1])
-				# Find Closest stop line waypoint index
-				d = temp_wp_idx - car_wp_idx
-				if d >= 0 and d < diff:
-					diff = d 
-					closest_light = light
-					line_wp_idx = temp_wp_idx
-			
+		car_wp_idx = self.get_closest_waypoint(self.pose.pose.position.x, self.pose.pose.position.y)
+		#TODO find the closest visible traffic light (if one exists)
+		diff = len(self.waypoints.waypoints)
+		for i, light in enumerate(self.ligths):
+			#Get stop line waypoint index
+			line = stop_line_positions[i]
+			temp_wp_idx = self.get_closest_waypoint(line[0], line[1])
+			# Find Closest stop line waypoint index
+			d = temp_wp_idx - car_wp_idx
+			if d >= 0 and d < diff:
+				diff = d
+				closest_light = light
+				line_wp_idx = temp_wp_idx
         if closest_light:
-            state = self.get_light_state(closest_light)
-            return light_wp, state
-			
+		state = self.get_light_state(closest_light)
+		return light_wp, state
         #self.waypoints = None
         return -1, TrafficLight.UNKNOWN
 
