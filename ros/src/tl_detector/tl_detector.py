82682020f0a6c29a2f71ae0080a931dadc4b611c
Clifton Pereira <pereiraclifton@gmail.com>
2018-04-07 12:42:14 -0400

    added control rate : 10 Hz to tl_detector

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -50,7 +50,32 @@ class TLDetector(object):
         self.last_wp = -1
         self.state_count = 0
 
-        rospy.spin()
+        #rospy.spin()
+		self.ros_spin()
+	
+	def ros_spin(self):
+		rate = rospy.Rate(10)
+		
+		while not rospy.is_shutdown():
+			'''
+			Publish upcoming red lights at camera frequency.
+			Each predicted state has to occur `STATE_COUNT_THRESHOLD` number
+			of times till we start using it. Otherwise the previous stable state is
+			used.
+			'''
+			if self.state != state:
+				self.state_count = 0
+				self.state = state
+			elif self.state_count >= STATE_COUNT_THRESHOLD:
+				self.last_state = self.state
+				light_wp = light_wp if state == TrafficLight.RED else -1
+				self.last_wp = light_wp
+				self.upcoming_red_light_pub.publish(Int32(light_wp))
+			else:
+				self.upcoming_red_light_pub.publish(Int32(self.last_wp))
+				self.state_count += 1
+			
+			rate.sleep()
 
     def pose_cb(self, msg):
         self.pose = msg
@@ -73,24 +98,7 @@ class TLDetector(object):
         self.camera_image = msg
         light_wp, state = self.process_traffic_lights()
 
-        '''
-        Publish upcoming red lights at camera frequency.
-        Each predicted state has to occur `STATE_COUNT_THRESHOLD` number
-        of times till we start using it. Otherwise the previous stable state is
-        used.
-        '''
-        if self.state != state:
-            self.state_count = 0
-            self.state = state
-        elif self.state_count >= STATE_COUNT_THRESHOLD:
-            self.last_state = self.state
-            light_wp = light_wp if state == TrafficLight.RED else -1
-            self.last_wp = light_wp
-            self.upcoming_red_light_pub.publish(Int32(light_wp))
-        else:
-            self.upcoming_red_light_pub.publish(Int32(self.last_wp))
-        self.state_count += 1
-
+        
     def get_closest_waypoint(self, pose_x, pose_y):
         """Identifies the closest path waypoint to the given position
             https://en.wikipedia.org/wiki/Closest_pair_of_points_problem
