8bf32139081adf634eea16c9cd80e90c2d8bfbd0
student <chongyou.ma@gmail.com>
2018-04-08 10:44:46 -0700

    Added CTE tracking and compensation

--- ros/src/twist_controller/dbw_node.py
+++ ros/src/twist_controller/dbw_node.py
@@ -3,8 +3,11 @@
 import rospy
 from std_msgs.msg import Bool
 from dbw_mkz_msgs.msg import ThrottleCmd, SteeringCmd, BrakeCmd, SteeringReport
-from geometry_msgs.msg import TwistStamped
+from geometry_msgs.msg import PoseStamped,TwistStamped
+from styx_msgs.msg import Lane, Waypoint
+from tf.transformations import euler_from_quaternion
 import math
+import numpy as np
 
 from twist_controller import Controller
 
@@ -57,7 +60,7 @@ class DBWNode(object):
         # self.controller = Controller(<Arguments you wish to provide>)
 		
         parameters = {
-                    'vehicle_mass':vehicle_mass,	
+                    'vehicle_mass':vehicle_mass,					
                     'fuel_capacity':fuel_capacity,					
                     'brake_deadband':brake_deadband,
                     'decel_limit':decel_limit,
@@ -74,6 +77,9 @@ class DBWNode(object):
         rospy.Subscriber('/vehicle/dbw_enabled',Bool,self.dbw_enabled_cb)
         rospy.Subscriber('/twist_cmd',TwistStamped,self.twist_cb)
         rospy.Subscriber('/current_velocity',TwistStamped,self.velocity_cb)
+        rospy.Subscriber('/current_pose', PoseStamped, self.pose_cb)
+        rospy.Subscriber('/final_waypoints', Lane, self.final_waypoints_cb)	
+		
         self.current_vel  = None
         self.curr_ang_vel = None
         self.dbw_enabled  = None
@@ -81,7 +87,9 @@ class DBWNode(object):
         self.angular_vel  = None
         self.throttle     = None
         self.steering     = None	
-        self.brake        = None		
+        self.brake        = None	
+        self.pose         = None		
+        self.final_waypoints_2d  = None				
         self.loop()
 
     def loop(self):
@@ -94,10 +102,10 @@ class DBWNode(object):
             #                                                     <current linear velocity>,
             #                                                     <dbw status>,
             #                                                     <any other argument you need>)
-            # if <dbw is enabled>:
-            #   self.publish(throttle, brake, steer)
+
             if not None in (self.current_vel, self.linear_vel, self.angular_vel):
-			    self.throttle, self.brake, self.steering = self.controller.control(self.current_vel, self.dbw_enabled,self.linear_vel,self.angular_vel)
+                cte = self.get_cte(self.final_waypoints_2d,self.pose)
+                self.throttle, self.brake, self.steering = self.controller.control(self.current_vel, self.dbw_enabled,self.linear_vel,self.angular_vel,cte)
 				
             if self.dbw_enabled:
 				self.publish(self.throttle, self.brake, self.steering)
@@ -110,7 +118,50 @@ class DBWNode(object):
         self.angular_vel = msg.twist.angular.z
 		
     def velocity_cb (self, msg):
-        self.current_vel  = msg.twist.linear.x		
+        self.current_vel  = msg.twist.linear.x	
+
+    def pose_cb(self, msg):
+        self.pose = msg
+        pass
+
+    def final_waypoints_cb(self, msg):
+        final_waypoints = msg.waypoints
+        self.final_waypoints_2d = [[waypoint.pose.pose.position.x, waypoint.pose.pose.position.y] for waypoint in final_waypoints]
+        pass	
+
+    def get_cte(self,final_waypoints_2d,current_pose):
+        if final_waypoints_2d is not None and current_pose is not None:		
+            #convert from quaternion to yaw
+            orientation_q = current_pose.pose.orientation
+            _, _, yaw = euler_from_quaternion ([orientation_q.x,orientation_q.y,orientation_q.z,orientation_q.w])	
+
+            #get the x and y of the current pose			
+            x = current_pose.pose.position.x
+            y = current_pose.pose.position.y
+            x_transform = []
+            y_transform = []		
+            cos_yaw = math.cos(-yaw)
+            sin_yaw = math.sin(-yaw)
+
+            #set the number of points to fit			
+            points_to_fit = 10
+            points_to_fit = min(points_to_fit,len(final_waypoints_2d))		
+			
+            #transform the waypoints to the vehicle frame
+            for i in range(points_to_fit):			
+                waypoint_x,waypoint_y = final_waypoints_2d[i]
+                x_d = waypoint_x - x
+                y_d = waypoint_y - y
+                x_transform.append(x_d *cos_yaw - y_d*sin_yaw)
+                y_transform.append(x_d *sin_yaw + y_d*cos_yaw)	
+
+            # Fit a 3rd degree polynomial to the waypoints
+            degree = 3
+            coefficients = np.polyfit(x_transform, y_transform, degree)
+
+            return np.polyval(coefficients, 0.0)				
+        else:
+            return 0               
 		
     def publish(self, throttle, brake, steer):
         tcmd = ThrottleCmd()
--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -11,68 +11,76 @@ ONE_MPH = 0.44704
 class Controller(object):
     def __init__(self, *args, **kwargs):
         # TODO: Implement
+		
+		#retrieve parameters
+        self.vehicle_mass   = kwargs['vehicle_mass']
+        self.fuel_capacity  = kwargs['fuel_capacity']
+        self.brake_deadband = kwargs['brake_deadband']
+        self.decel_limit    = kwargs['decel_limit']
+        self.accel_limit    = kwargs['accel_limit']
+        self.wheel_radius   = kwargs['wheel_radius']
+        self.wheel_base     = kwargs['wheel_base']		
+        self.steer_ratio    = kwargs['steer_ratio']
+        self.max_lat_accel  = kwargs['max_lat_accel']
+        self.max_steer_angle= kwargs['max_steer_angle']	
+        self.last_time      = rospy.get_time()
 
-        # Retrieve parameters.
-        self.vehicle_mass    = kwargs['vehicle_mass']
-        self.fuel_capacity   = kwargs['fuel_capacity']
-        self.brake_deadband  = kwargs['brake_deadband']
-        self.decel_limit     = kwargs['decel_limit']
-        self.accel_limit     = kwargs['accel_limit']
-        self.wheel_radius    = kwargs['wheel_radius']
-        self.wheel_base      = kwargs['wheel_base']
-        self.steer_ratio     = kwargs['steer_ratio']
-        self.max_lat_accel   = kwargs['max_lat_accel']
-        self.max_steer_angle = kwargs['max_steer_angle']
-
-        self.last_time = rospy.get_time()
-
-        self.yaw_controller = YawController(
-            self.wheel_base, self.steer_ratio, 0.1,
-            self.max_lat_accel, self.max_steer_angle)
+        self.yaw_controller = YawController(self.wheel_base, self.steer_ratio, 0.1, self.max_lat_accel, self.max_steer_angle)
 
         kp = 0.8
         ki = 0.0
         kd = 0.06
         throttle_min = 0
-        throttle_max = 0.5 * self.accel_limit
-        self.throttle_controller = PID(kp, ki, kd, throttle_min, throttle_max)
+        throttle_max = 0.5*self.accel_limit
+        self.throttle_controller = PID(kp, ki, kd,throttle_min,throttle_max)
 
-        tau = 0.5  # cutoff freq
-        ts  = 0.02  # sample time
+        kp_cte = 2.8
+        ki_cte = 0.1
+        kd_cte = 1.4		
+        self.cte_controller = PID(kp_cte, ki_cte, kd_cte,-self.max_steer_angle,self.max_steer_angle)		
+			
+        tau = 0.5 #cutoff freq
+        ts  = 0.02 #sample time
         self.vel_lowpass = LowPassFilter(tau,ts)
+		
+        pass
 
-    def control(self, current_vel, dbw_enabled, linear_vel, angular_vel):
+    def control(self, current_vel, dbw_enabled, linear_vel, angular_vel,cte):
         # TODO: Change the arg, kwarg list to suit your needs
         # Return throttle, brake, steer
-
+		
         if not dbw_enabled:
-            # Reset controller so that the integral term does not accumulate.
-            self.throttle_controller.reset();
-            return 0.0, 0.0, 0.0
-
-        current_vel = self.vel_lowpass.filt(current_vel)
-
-        steering = self.yaw_controller.get_steering(
-            linear_vel, angular_vel, current_vel)
-
+            #reset controller so that I term does not accumulate
+            self.throttle_controller.reset()
+            self.cte_controller.reset()
+            return 0.0,0.0,0.0
+        current_vel = self.vel_lowpass.filt(current_vel)		
+ 	
         vel_error = linear_vel - current_vel
 
-        current_time = rospy.get_time()
+        #calculate time elapsed from the previous time step
+        current_time = rospy.get_time()        
         sample_time  = current_time - self.last_time
         self.last_time = current_time
 
         throttle = self.throttle_controller.step(vel_error, sample_time)
 
+        #calculate the additional steering control due to CTE Error and add it to the base 
+        steering_base = self.yaw_controller.get_steering(linear_vel, angular_vel, current_vel)
+        steering_cte  = self.cte_controller.step(cte, sample_time)	
+        steering_total= steering_base + steering_cte
+        steering = max(min(self.max_steer_angle,steering_total),-self.max_steer_angle)
+		
         brake = 0
 
         if linear_vel == 0 and current_vel < 0.1:
             throttle = 0
-            brake = 450  # Nm - for car to be held stationary
-
-        elif throttle < 0.1 and vel_error < 0:
-            throttle = 0
-            # Ensure deceleration does not exceed decel_limit.
+            brake = 450 # Nm - for car to be held stationary
+		
+        elif throttle < 0.1 and vel_error < 0:	
+            throttle = 0          
+            #ensure deceleration does not exceed decel_limit			
             decel = max(vel_error, self.decel_limit)
-            brake = abs(decel) * (self.vehicle_mass + self.fuel_capacity * GAS_DENSITY) * self.wheel_radius  # Torque
-
-        return throttle, brake, steering
+            brake = abs(decel)*(self.vehicle_mass + self.fuel_capacity * GAS_DENSITY)*self.wheel_radius #Torque
+			
+        return throttle, brake , steering
